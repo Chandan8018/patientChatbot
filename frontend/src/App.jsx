6c352f6a741a35a6264504897b1564a43684717e
Chandan8018 <chandankumarsahoo19@gmail.com>
2024-07-18 08:39:08 +0530

    Complete header and footer components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,23 @@
-import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/header/Header";
+import FooterComp from "./components/footer/FooterComp";
+import Home from "./pages/Home";
+import Contact from "./pages/Contact";
+// import { lazy } from "react";
 
+// const Contact = lazy(() => import("./pages/Contact"));
 function App() {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path='*' element={<Home />} />
+        <Route path='/' element={<Home />} />
+        <Route path='/contact' element={<Contact />} />
+      </Routes>
+      <FooterComp />
+    </BrowserRouter>
+  );
 }
 
 export default App;
